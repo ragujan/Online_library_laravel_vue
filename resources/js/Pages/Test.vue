f6d89d6041f84f01161f2509e752b08ab310b9a0
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2024-07-19 01:22:34 +0530

    test: random vue component test with routes, controllers

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const count = ref(0);
+function increment(){
+    count.value++;
+}
+</script>
+<template>
+    <button class="bg-red-600 p-4" @click="increment">
+        {{ count }}
+    </button>
+</template>
